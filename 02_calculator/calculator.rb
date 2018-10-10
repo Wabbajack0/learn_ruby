f013c6a48cbe690e2cd6160b1f6880705f0c24e8
Edoardo Rota <edoardorota96@gmail.com>
2018-10-10 11:13:34 +0200

    completed 02_calculator also with bonus questions

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
-#write your code here
+def add (a, b)
+  a + b
+end
+
+def subtract (a, b)
+  a - b
+end
+
+def sum array
+  result = 0
+  array.each do |i|
+    result += i
+  end
+  result
+end
+
+def multiply (a, b, *other_numbers)
+  res = a * b
+  if other_numbers
+    other_numbers.each do |i|
+      res *= i
+    end
+  end
+  res
+end
+
+def power (a, b)
+  a ** b
+end
+
+def factorial a
+  i = 1
+  a.times do
+    i *= a
+    a -= 1
+  end
+  i
+end
