921bb2a32425948cb205a790a843ac26d27ae0bd
Edoardo Rota <edoardorota96@gmail.com>
2018-10-10 17:34:51 +0200

    Completed 05_book_titles and 06_timer

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+  attr_accessor :title
+  def title()
+    words_array = @title.split
+    words_array.each_index do |i|
+      if check?(words_array[i]) or i == 0
+        words_array[i] = words_array[i].capitalize
+      end
+    end
+    return words_array.join(" ")
+  end
+
+  private
+  def check?(str)
+    non_caps_words = ["and", "in", "the", "of", "a", "an"]
+    return !non_caps_words.include?(str)
+  end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,28 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds / 3600
+    @seconds -= hours * 3600
+    minutes = @seconds / 60
+    @seconds -= minutes * 60
+    hours = hours.to_s
+    minutes = minutes.to_s
+    @seconds = @seconds.to_s
+    if hours.length == 1
+      hours = hours.insert(0, "0")
+    end
+    if minutes.length == 1
+      minutes = minutes.insert(0, "0")
+    end
+    if @seconds.length == 1
+      @seconds = @seconds.insert(0, "0")
+    end
+    return hours.to_s + ":" + minutes.to_s + ":" + @seconds.to_s
+  end
+
 end
